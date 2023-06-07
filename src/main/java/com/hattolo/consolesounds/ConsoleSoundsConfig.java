991423cb267f4faeb6e6ea33e5e04badf81ed4fa
S_N00B <49575478+S-N00B-1@users.noreply.github.com>
2023-06-07 23:20:15 +0000

    A

--- src/main/java/com/hattolo/consolesounds/ConsoleSoundsConfig.java
+++ src/main/java/com/hattolo/consolesounds/ConsoleSoundsConfig.java
@@ -2,6 +2,7 @@ package com.hattolo.consolesounds;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "consolesounds")
